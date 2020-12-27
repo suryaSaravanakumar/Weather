ab09c530a55821969a9fd18b016a6608b9763481
Surya SaravanaKumar <surya@agiliztech.com>
2020-12-27 13:37:47 +0530

    Added NetworkHelper

---
+++ Weather/Shared/Constant/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  Weather
+//
+//  Created by Surya on 27/12/20.
+//
+
+import Foundation
+
+struct ApiKey {
+    static let apiKey = "9bf9678ebb19ac14d76e33f3c3f93621"
+}
---
+++ Weather/Shared/Network Helper/Network Helper.swift
@@ -0,0 +1,44 @@
+//
+//  Network Helper.swift
+//  Weather
+//
+//  Created by Surya on 27/12/20.
+//
+
+import Foundation
+
+class NetwokHelper{
+    static let sharedInstance = NetwokHelper()
+    private init (){}
+    
+    let session = URLSession.shared
+    
+    /**
+     Call this function to use get API
+     - Parameter endPoint: The URL Endpoint for the current API
+     - Parameter completion: Completion closure contains the genric type of Codeable model
+        - T  - Codeable Model (Pass your model here) to get the response from the API
+     - Parameter failureBlock: Failure block with error
+     */
+    func callGetAPIWith<T: Codable>(endPoint:String,
+                                       completion: @escaping (T) -> (),
+                                       failureBlock:@escaping (String) ->()){
+        guard  let url = URL(string: endPoint) else { return  }
+        
+        //FIXME: - Handle No Internet case
+        let task = session.dataTask(with: url, completionHandler: { data, response, error in
+            
+            if error != nil {
+                failureBlock(error.debugDescription)
+                return
+            }
+            do {
+                let json = try JSONDecoder().decode(T.self, from: data! )
+                completion(json)
+            } catch {
+                print("Error during JSON serialization: \(error.localizedDescription)")
+            }
+        })
+        task.resume()
+    }
+}
